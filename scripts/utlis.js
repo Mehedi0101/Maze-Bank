e381f5e0b500568c73f961ec730291f790b1863c
Mehedi0101 <mehedi29121998@gmail.com>
2023-08-19 08:43:39 +0600

    register page removed

---
+++ scripts/utlis.js
@@ -0,0 +1,3 @@
+const registerBtn = document.getElementById('register-btn');
+const caEmailField = document.getElementById('ca-email-field');
+const caPasswordField = document.getElementById('ca-password-field');
