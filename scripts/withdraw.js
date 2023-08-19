d6ca0dc4c733049ee7a2e02783e0f26e307a6afe
Mehedi0101 <mehedih2909@gmail.com>
2023-08-19 10:58:58 +0600

    some function prototypes moved to the utils file

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,15 +1,3 @@
-function withdrawFunctionalities(){
-    if(validation(withdrawField,invalidWithdraw)){
-        insufficientWithdraw.classList.add('hidden');
-        return;
-    }
-    if(validationSufficiency(withdrawField,balanceAmount,insufficientWithdraw)){
-        return;
-    }
-    depositUpdate(withdrawField,withdrawAmount);
-    withdrawUpdate(withdrawField,balanceAmount);
-    withdrawField.value = '';
-}
 withdrawButton.addEventListener('click',function(){
     withdrawFunctionalities();
 })
